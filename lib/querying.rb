645b24e37da3d7c88dee8f5fb4012fe6e5d55721
Eden Mesfun <emesfun@gmail.com>
2018-03-13 18:01:13 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -40,6 +40,6 @@ def select_character_names_and_number_of_books_they_are_in
   FROM characters
   INNER JOIN character_books
   WHERE characters.id = character_books.character_id
-  GROUP BY characters.name DESC
+  GROUP BY characters.name
   ;"
 end
