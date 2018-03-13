af28d1699b37d7a4aec5f6dbd9185a64f4278e1a
Eden Mesfun <emesfun@gmail.com>
2018-03-13 15:36:31 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,4 +1,4 @@
-INSERT INTO series VALUES();
+INSERT INTO series VALUES(1, harry potter, jk_rowling, fantasy);
 
 CREATE TABLE series
 (id INTEGER PRIMARY KEY, title TEXT, author_id TEXT, subgenre_id TEXT);
