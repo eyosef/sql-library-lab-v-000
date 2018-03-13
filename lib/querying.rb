e3f195216772eda091aa44c0d92c6dcefdaceee9
Eden Mesfun <emesfun@gmail.com>
2018-03-13 16:30:53 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,5 +1,7 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "SELECT books.title, books.year"
+  "SELECT books.title, books.year
+  ON books
+  ORDER BY books.year ASC;"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
