a2f0f123ff0f27a1da7cd9e1831bd9f1fd5460af
Eden Mesfun <emesfun@gmail.com>
2018-03-13 15:49:17 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -8,7 +8,7 @@ INSERT INTO books VALUES(4, bad_beginning, 1999, 2);
 INSERT INTO books VALUES(5, reptile_room, 1999, 2);
 INSERT INTO books VALUES(6, wide_window, 2000, 2);
 
-INSERT INTO characters VALUES (1, hpotter, human, courage, 1, 1 );
+INSERT INTO characters VALUES (1, hpotter, human, courage, 1, 1);
 
 CREATE TABLE characters
 (id INTEGER PRIMARY KEY, name TEXT, species TEXT, motto TEXT, series_id TEXT, author_id TEXT);
