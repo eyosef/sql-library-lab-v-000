0235ac86d215e74a016a891326989f7877ad1dd9
Eden Mesfun <emesfun@gmail.com>
2018-03-13 17:19:45 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -27,7 +27,7 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title, COUNT(characters.species)
+  "SELECT series.title, characters.species
   FROM series
   INNER JOIN characters
   WHERE characters.species = 'human'
