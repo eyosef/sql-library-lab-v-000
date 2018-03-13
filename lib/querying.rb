40834a63ff8fb5d12a57f6fe7cc7250a45fc7735
Eden Mesfun <emesfun@gmail.com>
2018-03-13 18:13:22 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -41,6 +41,5 @@ def select_character_names_and_number_of_books_they_are_in
   INNER JOIN character_books
   WHERE characters.id = character_books.character_id
   ORDER BY COUNT(character_books.character_id)
-  GROUP BY characters.name
-  ;"
+  GROUP BY characters.name;"
 end
