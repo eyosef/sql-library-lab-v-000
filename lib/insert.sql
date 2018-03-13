891fb61e6c01d630b467b7ebc0b4a7621ff78dde
Eden Mesfun <emesfun@gmail.com>
2018-03-13 15:41:12 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,6 +1,8 @@
 INSERT INTO series VALUES(1, harry potter, jk_rowling, fantasy);
 INSERT INTO series VALUES(2, unfortunate_series, lsnicket, fantasy);
 
+INSERT INTO books VALUES();
+
 
 INSERT INTO authors VALUES(1, jkrowling);
 INSERT INTO authors VALUES(2, lsnicket);
