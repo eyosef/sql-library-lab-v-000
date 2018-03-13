183a5da63caf51a9bcab15da78c9cd2da5cb87ca
Eden Mesfun <emesfun@gmail.com>
2018-03-13 15:41:39 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,7 +1,7 @@
 INSERT INTO series VALUES(1, harry potter, jk_rowling, fantasy);
 INSERT INTO series VALUES(2, unfortunate_series, lsnicket, fantasy);
 
-INSERT INTO books VALUES();
+INSERT INTO books VALUES(1, );
 
 
 INSERT INTO authors VALUES(1, jkrowling);
@@ -9,9 +9,6 @@ INSERT INTO authors VALUES(2, lsnicket);
 
 
 
+CREATE TABLE books
+(id INTEGER PRIMARY KEY, title TEXT, year INTEGER, series_id INTEGER);
 
-CREATE TABLE series
-(id INTEGER PRIMARY KEY, title TEXT, author_id TEXT, subgenre_id TEXT);
-
-CREATE TABLE authors
-(id INTEGER PRIMARY KEY, name TEXT);
