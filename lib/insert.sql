8db486f7ea3d9b2d1da8ba6dd26e5c16b708aae4
Eden Mesfun <emesfun@gmail.com>
2018-03-13 15:51:43 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -14,7 +14,7 @@ INSERT INTO characters VALUES (3, rweasley, human, humor, 1, 1);
 INSERT INTO characters VALUES (4, dumbledore, human, smart, 1, 1);
 
 INSERT INTO characters VALUES (5, violetb, human, inventive, 2, 2);
-INSERT INTO characters VALUES (5, violetb, human, inventive, 2, 2);
+INSERT INTO characters VALUES (6, klausb, human, inventive, 2, 2);
 
 CREATE TABLE characters
 (id INTEGER PRIMARY KEY, name TEXT, species TEXT, motto TEXT, series_id TEXT, author_id TEXT);
