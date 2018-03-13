123e7e3c8cad2aa864bbc7a1fa30af0dc85b0264
Eden Mesfun <emesfun@gmail.com>
2018-03-13 17:52:17 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -36,9 +36,8 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT characters.name, COUNT(character_books.character_id)
+  "SELECT characters.name
   FROM characters
-  INNER JOIN character_books
   ORDER BY characters.name
   ;"
 end
