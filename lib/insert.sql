af978ef7590448c22905e560e0396ed2b40de874
Eden Mesfun <emesfun@gmail.com>
2018-03-13 15:46:19 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -6,7 +6,7 @@ INSERT INTO books VALUES(2, chamber_of_secrets, 1999, 1);
 INSERT INTO books VALUES(3, prisoner_of_az, 1999, 1);
 INSERT INTO books VALUES(4, bad_beginning, 1999, 2);
 INSERT INTO books VALUES(5, reptile_room, 1999, 2);
-INSERT INTO books VALUES(6, wide_window, 1999, 2);
+INSERT INTO books VALUES(6, wide_window, 2000, 2);
 
 INSERT INTO authors VALUES(1, jkrowling);
 INSERT INTO authors VALUES(2, lsnicket);
