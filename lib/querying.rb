dc59643f9f296316862739904b21df102f6b4e0f
Eden Mesfun <emesfun@gmail.com>
2018-03-13 16:48:52 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -15,7 +15,7 @@ end
 def select_value_and_count_of_most_prolific_species
   "SELECT COUNT(characters.species)
   FROM characters
-  ORDER BY characters.species COUNT;"
+  ORDER BY characters.species;"
 end
 
 def select_name_and_series_subgenres_of_authors
