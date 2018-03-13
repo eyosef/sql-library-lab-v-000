07acd8ba036cc605defb0c51aa41aa1414dc7373
Eden Mesfun <emesfun@gmail.com>
2018-03-13 17:26:22 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -32,6 +32,7 @@ def select_series_title_with_most_human_characters
   INNER JOIN characters
   ON series.author_id = characters.author_id
   WHERE characters.species = 'human'
+  ORDER BY series.title DESC
   ;"
 end
 
