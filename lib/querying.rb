9484ee08c3de1eac898f9868cafce3df0a0eabd0
Eden Mesfun <emesfun@gmail.com>
2018-03-13 16:36:52 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,7 +1,6 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "SELECT books.title, books.year
+  "SELECT books.title, books.year   WHERE books.series = 1
   FROM books
-  WHERE books.series = 1
   ORDER BY books.year ASC;"
 end
 
