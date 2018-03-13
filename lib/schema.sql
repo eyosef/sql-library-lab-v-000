1a50239dd51f8f3a0feaa22e814790eaf3d0e31a
Eden Mesfun <emesfun@gmail.com>
2018-03-13 15:32:33 +0000

    automatically backed up by learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -1,5 +1,5 @@
 CREATE TABLE series
-(id PRIMARY KEY, title TEXT, author_id TEXT, subgenre_id TEXT);
+(id INTEGER PRIMARY KEY, title TEXT, author_id TEXT, subgenre_id TEXT);
 
 CREATE TABLE subgenres
 (id PRIMARY KEY, name TEXT);
